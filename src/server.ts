d384c9b682a4dfd8ea4ee6c6722467fb1a6bb286
Mateus Nadal <nadalmateus@outlook.com>
2021-05-03 23:06:19 -0300

    perf(helmet): security layer

--- src/server.ts
+++ src/server.ts
@@ -2,9 +2,11 @@ import express from 'express'
 import routes from './routes'
 import 'reflect-metadata'
 import './database/databaseConnection'
+import helmet from 'helmet'
 
 const app = express()
 app.use(express.json())
+app.use(helmet())
 
 app.use(routes)
 
